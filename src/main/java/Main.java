585a35a6d77d6b1ae18259d4e89d210c7abe7fc4
apodvysotskiy <a.podvysotskiy@nicetu.spb.ru>
2023-06-19 16:01:10 +0300

    feat: add FileManager

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,20 +11,18 @@ public class Main {
         //List<String> lines = new ArrayList<>();
         //lines.add("4.0, 1976091.0, 3974537.2, 4564998.2, 1.671, 4.251, 4.931");
         //lines.add("5.0, 2976091.0, 4974537.2, 5564998.2, 2.671, 5.251, 5.931");
+        //Trajectory newTrajectory = new Trajectory(lines, "/Users/andrejpodvysockij/Desktop/traject1.txt");
 
         FileService fileService = new FileService();
         Trajectory trajectory = fileService.openFile();
         //fileService.renameFile();
-        fileService.editFile(trajectory);
+        //fileService.editFile(newTrajectory);
 
-        //Trajectory trajectory = new Trajectory(lines, "/Users/andrejpodvysockij/Desktop/traject1.txt");
-
-        /*for(int i = 0; i < trajectory.getLinesCount(); i++){
+        for(int i = 0; i < trajectory.getLinesCount(); i++){
             System.out.println("Время: " + trajectory.getTime(i));
             System.out.println("Координаты: " + trajectory.getCoordinates(i));
             System.out.println("Скорость: " + trajectory.getSpeed(i));
         }
-
-        System.out.println("Источник: " + trajectory.getPath() + ", " + trajectory.getFileName());*/
+        System.out.println("Источник: " + trajectory.getPath() + ", " + trajectory.getFileName());
     }
 }
